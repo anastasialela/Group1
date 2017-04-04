9485e2e115efd498867a97a70218a9d04c6e6bdd
zarith lela <zarith lela>
2017-04-04 13:37:40 +0800

    Commit Message

---
+++ app/src/main/java/com/example/hp/rent4me/Rent4me.java
@@ -0,0 +1,14 @@
+package com.example.hp.rent4me;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Rent4me extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rent4me);
+    }
+}
+`
